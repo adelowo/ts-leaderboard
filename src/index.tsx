a48a342c88ab574e462adc36b7544ed3e64d1565
Lanre Adelowo <me@lanreadelowo.com>
2017-05-27 07:44:46 +0100

    Application entry files

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './Component/App';
+// import AxiosClient from './Services/Http/axios';
+import MockClient from './Services/Http/mock';
+
+ReactDOM.render(
+  <App client={new MockClient()}/>,
+  document.getElementById('root') as HTMLElement
+);
