6a1f9daca1666ceaba8b4d04d3703b2f7d0d2a18
Lanre Adelowo <me@lanreadelowo.com>
2017-05-27 08:02:39 +0100

    Enhanced presentation

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import './index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './Component/App';
-// import AxiosClient from './Services/Http/axios';
-import MockClient from './Services/Http/mock';
+import AxiosClient from './Services/Http/axios';
+//import MockClient from './Services/Http/mock';
 
 ReactDOM.render(
-  <App client={new MockClient()}/>,
+  <App client={new AxiosClient()}/>,
   document.getElementById('root') as HTMLElement
 );
